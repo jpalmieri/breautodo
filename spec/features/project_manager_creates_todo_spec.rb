a04b162a307ffdd6050bcaa78408ceb4d742216d
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-04 23:09:07 +0000

    create todo spec wasn't failing when it should have. added sign in action so that it does

--- spec/features/project_manager_creates_todo_spec.rb
+++ spec/features/project_manager_creates_todo_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 feature 'Project manager creates TODO' do
   scenario 'Successfully' do
+    user = create(:user)
+    visit root_path
+
+    within '.user-info' do
+      click_link 'Sign In'
+    end
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    within 'form' do
+      click_button 'Sign in'
+    end
+
     visit new_todo_path
     fill_in 'Description', with: 'Meet up with the team'
     click_button 'Save'
+
     expect( page ).to have_content('Your new TODO was saved')
     expect( page ).to have_content('Meet up with the team')
   end
