fa119d3f5b669ddbdfa5a05c505cbe1d75fa5171
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-04 00:05:25 +0000

    added spec for sign up: password too short

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -19,5 +19,24 @@ feature 'User signs up' do
     expect(current_path).to eq root_path
     expect( page ).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
   end
+
+  scenario "unsuccessfully" do
+    visit root_path
+    within '.user-info' do
+      click_link "Sign Up"
+    end
+
+    fill_in 'Name', with: "Robert Paulson"
+    fill_in 'Email', with: "robertpaulson@example.com"
+    fill_in 'Password', with: "1234567"
+    fill_in 'Password confirmation', with: "1234567"
+
+    within 'form' do
+      click_button 'Sign up'
+    end
+
+    expect(current_path).to eq user_registration_path
+    expect( page ).to have_content("1 error prohibited this user from being saved: Password is too short (minimum is 8 characters)")
+  end
  
 end
