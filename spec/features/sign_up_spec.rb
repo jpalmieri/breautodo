b74484fcb074b47e840c463b407ed13774e9cb27
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-04 00:14:09 +0000

    refactored sign up spec

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -7,13 +7,17 @@ feature 'User signs up' do
     within '.user-info' do
       click_link 'Sign Up'
     end
+    fill_in 'Name', with: "Robert Paulson"
+    @valid_email = "robertpaulson@example.com"
+    @invalid_email = "robertpaulson@example"
+    @valid_password = "hellowworld"
+    @invalid_password = "1234567"
   end
 
   scenario 'Successfully' do
-    fill_in 'Name', with: "Robert Paulson"
-    fill_in 'Email', with: "robertpaulson@example.com"
-    fill_in 'Password', with: "helloworld"
-    fill_in 'Password confirmation',with: "helloworld"
+    fill_in 'Email', with: @valid_email
+    fill_in 'Password', with: @valid_password
+    fill_in 'Password confirmation',with: @valid_password
     
     within 'form' do
       click_button 'Sign up'
@@ -24,10 +28,9 @@ feature 'User signs up' do
   end
 
   scenario "Password too short" do
-    fill_in 'Name', with: "Robert Paulson"
-    fill_in 'Email', with: "robertpaulson@example.com"
-    fill_in 'Password', with: "1234567"
-    fill_in 'Password confirmation', with: "1234567"
+    fill_in 'Email', with: @valid_email
+    fill_in 'Password', with: @invalid_password
+    fill_in 'Password confirmation', with: @invalid_password
 
     within 'form' do
       click_button 'Sign up'
@@ -38,10 +41,9 @@ feature 'User signs up' do
   end
 
   scenario "Invalid email" do
-    fill_in 'Name', with: "Robert Paulson"
-    fill_in 'Email', with: "robertpaulson@example"
-    fill_in 'Password', with: "helloworld"
-    fill_in 'Password confirmation', with: "helloworld"
+    fill_in 'Email', with: @invalid_email
+    fill_in 'Password', with: @valid_password
+    fill_in 'Password confirmation', with: @valid_password
 
     within 'form' do
       click_button 'Sign up'
