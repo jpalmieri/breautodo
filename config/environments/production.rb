407adf342f95cb6372e5c50b18761552479a157e
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-04 19:57:15 +0000

    updated production action mailer url for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Blocitoff::Application.configure do
+
+  config.action_mailer.default_url_options = { :host => 'blocitoff-jpalmieri.heroku.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
