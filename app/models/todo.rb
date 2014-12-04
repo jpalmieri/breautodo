df7c3be8dab74fe4aa00848f7dae283880e8b17c
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-04 20:19:47 +0000

    fixed bug: creating todos from todos/new was not saving todo with user_id

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,7 @@
 class Todo < ActiveRecord::Base
   belongs_to :user
 
-validates :description, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 5 }, presence: true
+  validates :user_id, presence: :true
 
 end
