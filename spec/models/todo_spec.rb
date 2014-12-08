5d89b90fb96c78345e8db34bfa495db2b8483b02
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-08 23:51:21 +0000

    spec for todos destruction

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
+require 'rake'
 
 describe Todo do
   
+  before do
+    @user = create(:user)
+    @todo = create(:todo, user: @user)
+  end
+
   describe "#days_left" do
-    before do
-      @todo = create(:todo)
-    end
 
     it "counts the number of days until a new todo is destroyed" do
       expect( @todo.days_left.round ).to eq(7)
@@ -13,4 +16,27 @@ describe Todo do
 
   end
 
+  context "delete_items rake task" do
+    it "destroys todo after 7 days" do
+      Timecop.freeze(Time.now + 7.days) do
+        #load and execute custom rake task
+        load File.expand_path("../../../lib/tasks/remove_outdated_todos.rake", __FILE__)
+        Rake::Task.define_task(:environment)
+        Rake::Task["delete_items"].invoke
+
+        expect( @user.todos.first ).to be_nil
+      end
+    end
+
+    it "does not destroy todo after 6 days" do
+      Timecop.freeze(Time.now + 6.days) do
+        #load and execute custom rake task
+        load File.expand_path("../../../lib/tasks/remove_outdated_todos.rake", __FILE__)
+        Rake::Task.define_task(:environment)
+        Rake::Task["delete_items"].invoke
+
+        expect( @user.todos.first ).to_not be_nil
+      end
+    end
+  end
 end
