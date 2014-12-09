d10a9de867122693e7f3c20e6542ac86313ab8de
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-09 22:58:50 +0000

    moved remove_outdated_todos to its own task spec

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
-require 'rake'
 
 describe Todo do
   
   before do
-    @user = create(:user)
-    @todo = create(:todo, user: @user)
+    @todo = create(:todo)
   end
 
   describe "#days_left" do
@@ -14,29 +12,5 @@ describe Todo do
       expect( @todo.days_left.round ).to eq(7)
     end
 
-  end
-
-  context "delete_items rake task" do
-    it "destroys todo after 7 days" do
-      Timecop.freeze(Time.now + 7.days) do
-        #load and execute custom rake task
-        load File.expand_path("../../../lib/tasks/remove_outdated_todos.rake", __FILE__)
-        Rake::Task.define_task(:environment)
-        Rake::Task["delete_items"].invoke
-
-        expect( @user.todos.first ).to be_nil
-      end
-    end
-
-    it "does not destroy todo after 6 days" do
-      Timecop.freeze(Time.now + 6.days) do
-        #load and execute custom rake task
-        load File.expand_path("../../../lib/tasks/remove_outdated_todos.rake", __FILE__)
-        Rake::Task.define_task(:environment)
-        Rake::Task["delete_items"].invoke
-
-        expect( @user.todos.first ).to_not be_nil
-      end
-    end
   end
 end
