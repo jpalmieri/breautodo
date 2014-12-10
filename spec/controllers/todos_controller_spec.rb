76e3e6be534ec3a4bcbbdb40915ca1e9236f541a
jpalmieri <jpalmieri@lumoslabs.com>
2014-12-10 05:50:20 +0000

    created specs for todo#destroy. Modified NotAuthorizedError

---
+++ spec/controllers/todos_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe TodosController do
+
+  include Devise::TestHelpers
+
+  before do
+    @user1 = create(:user)
+    @user2 = create(:user)
+    @todo = create(:todo, user: @user1)
+  end
+
+  describe '#destroy' do
+    it "allows a user to delete own todo" do
+      sign_in @user1
+      delete :destroy, { id: @todo.id }
+      should redirect_to todos_url
+      should set_the_flash.to("Todo deleted successfully")
+    end
+
+    it "does not allow a user to delete another user's todo" do
+      sign_in @user2
+      delete :destroy, { id: @todo.id }
+      should redirect_to root_url
+      should set_the_flash.to("You are not authorized")
+    end
+
+  end
+
+end
