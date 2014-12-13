9e2ed05879cb4701d0ad1d245135cfe61e61446c
Joe Palmieri <jpalmieri@lumoslabs.com>
2014-12-12 16:00:06 -0800

    added spec for no zeros to be shown on todos#index

--- spec/features/todo_list_spec.rb
+++ spec/features/todo_list_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 feature "View multiple todo items" do
-  scenario "successfilly" do
+
+  before do
     user = create(:user)
     sign_in(user)
 
@@ -12,10 +13,20 @@ feature "View multiple todo items" do
     visit new_todo_path
     fill_in 'Description', with: "Get haircut"
     click_button 'Save'
+  end
+
+  scenario "successfully" do
 
     expect(current_path).to eq(todos_path)
 
     expect( page ).to have_content("Buy groceries")
     expect( page ).to have_content("Get haircut")
+  end
+
+  scenario "without seeing '0' in 'days left' column" do
+    Timecop.freeze(Time.now + 6.5.days) do
+      visit todos_path
+      expect( first('.days-left') ).to_not have_content("0")
+    end
   end
 end
