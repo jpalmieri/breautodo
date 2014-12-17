437ea8a8f5f52b76cf363160d8a67908b75a98be
Joe Palmieri <jpalmieri@lumoslabs.com>
2014-12-17 11:44:29 -0800

    added validation for auth_token to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ describe User do
     it { expect(User.new).to have_many(:todos) }
   end
 
+  context "validations" do
+    it { expect(User.new).to validate_presence_of(:auth_token) }
+  end
+
 end
