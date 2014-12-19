ca3e78ac173fe00a6e21c12807d2b45d34ec9ccb
Joe Palmieri <jpalmieri@lumoslabs.com>
2014-12-19 00:19:56 -0800

    WIP added additional api#destroy logic; still need to add auth tokens

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,13 +1,24 @@
 class Api::V1::TodosController < Api::ApiController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def destroy
+    todo = Todo.find(params[:id])
     user = User.find_by_auth_token(request.headers["Authorization"])
-    if user
-      Todo.find(params[:id]).destroy
-      head :no_content
-    else
+
+    if !user
       render json: {message: "Unauthorized"}, status: :unauthorized
+    elsif user != todo.user
+      render json: {message: "Forbidden"}, status: :forbidden
+    else
+      todo.destroy
+      head :no_content
     end
+  end
+
+  private
+
+  def not_found_response
+    render json: {message: "Not Found"}, status: :not_found
   end
 
 end
