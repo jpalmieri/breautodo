718d80448fd7aefa11e438cb363641e80873d9be
Joe Palmieri <jpalmieri@lumoslabs.com>
2015-01-12 15:44:56 -0800

    added notification for delete todo

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -38,7 +38,11 @@
       // Assumes that the delete button is a child element of the
       // todo's table row.
       var todoRow = clickedElement.closest("tr");
-      todoRow.remove();
+      todoRow.html('<div class="alert alert-success">Todo deleted successfully</div>').fadeOut(1000);
+    };
+
+    function todoDeletedFlash() {
+      
     };
 
     $.ajax(ajaxOptions).done(removeTodoFromDOM).fail(handleError);
