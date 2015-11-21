677c99644277456c88fc9ce8964ad1713c0bba7d
jpalmieri <jpalmieri@lumoslabs.com>
2015-11-21 10:21:27 -0800

    redirect root to login if not logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if user_signed_in?
       redirect_to todos_path
+    else
+      redirect_to new_user_session_path
     end
   end
 end
