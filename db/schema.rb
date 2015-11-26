ccba544799aa3be6ae68ef17d0ae91c9b922b440
Joe Palmieri <jpalmieri@lumoslabs.com>
2015-11-25 21:28:50 -0800

    add list reference to todos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126044429) do
+ActiveRecord::Schema.define(version: 20151126051937) do
 
   create_table "lists", force: true do |t|
     t.string   "name",       null: false
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20151126044429) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "list_id"
   end
 
+  add_index "todos", ["list_id"], name: "index_todos_on_list_id"
   add_index "todos", ["user_id"], name: "index_todos_on_user_id"
 
   create_table "users", force: true do |t|
