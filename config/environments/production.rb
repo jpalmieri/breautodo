4604a26d7ff658990458afa904a5fe6290b2d2f3
Joe Palmieri <jpalmieri@lumoslabs.com>
2016-03-10 07:55:37 -0800

    change production default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Blocitoff::Application.configure do
 
-  config.action_mailer.default_url_options = { :host => 'blocitoff-jpalmieri.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'blocitoff-api-jpalmieri.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
