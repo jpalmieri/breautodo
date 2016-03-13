1c6eb6a163ca3cdc55d3260af966bff0f457dae6
Joe Palmieri <jpalmieri@lumoslabs.com>
2016-03-13 15:17:27 -0700

    add test for list with new todo is first in list index

--- spec/features/user_updates_list_spec.rb
+++ spec/features/user_updates_list_spec.rb
@@ -4,13 +4,13 @@ feature 'User updates list', js: true do
 
   before do
     user = create(:user)
-    list1 = create(:list, user: user, name: 'List one')
-    list2 = create(:list, user: user, name: 'List two')
-    list3 = create(:list, user: user, name: 'List three')
+    @list1 = create(:list, user: user, name: 'List one')
+    @list2 = create(:list, user: user, name: 'List two')
+    @list3 = create(:list, user: user, name: 'List three')
     sign_in(user)
   end
 
-  scenario 'successfully' do
+  scenario 'successfully: update name' do
     within find('#lists tr:last-child') do
       click_button('Edit')
     end
@@ -22,7 +22,22 @@ feature 'User updates list', js: true do
     expect( find('#lists tr:first-child') ).to have_content('updated list')
   end
 
-  scenario 'Unsuccessfully: no description' do
+  scenario 'successfully: with new todo' do
+    within find('#lists tr:last-child td.name') do
+      click_link @list1.name
+    end
+
+    fill_in 'Description', with: "new todo"
+    click_button 'Add Todo'
+
+    expect(current_path).to eq(list_path @list1)
+    click_link "Lists"
+
+    # updated list is first
+    expect( find('#lists tr:first-child') ).to have_content(@list1.name)
+  end
+
+  scenario 'Unsuccessfully: no name' do
     within find('#lists tr:last-child') do
       click_button('Edit')
     end
