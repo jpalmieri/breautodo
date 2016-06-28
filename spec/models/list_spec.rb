7158cb75a531f35b4327aea85c210c590b4d59d3
jpalmieri <jpalmieri@lumoslabs.com>
2016-06-28 08:32:40 -0700

    convert list model spec to use 'should' syntax

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,23 +1,13 @@
 require 'rails_helper'
 
 describe List do
-
-  before do
-    @list = create(:list)
-    5.times do
-      create(:todo, list: @list)
-    end
-  end
-
   context 'associations' do
-    it { expect(List.new).to belong_to(:user) }
-    it { expect(List.new).to have_many(:todos) }
+    it { should belong_to(:user) }
+    it { should have_many(:todos) }
   end
 
   context "validations" do
-    it { expect(List.new).to validate_presence_of(:name) }
-    it { expect(List.new).to validate_presence_of(:user_id) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:user_id) }
   end
-
-
 end
