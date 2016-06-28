7ec74f6ea6bfe6cefc6d45219b4cdaa40a786d0a
jpalmieri <jpalmieri@lumoslabs.com>
2016-06-27 22:00:06 -0700

    add :newest scope to todos

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Todo do
+  let!(:first_todo)  { create(:todo, description: 'first todo') }
+  let!(:second_todo) { create(:todo, description: 'second todo') }
 
   describe 'associations' do
     it { should belong_to(:user) }
@@ -10,5 +12,9 @@ describe Todo do
   context 'validations' do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:user_id) }
+  end
+
+  context 'when :newest scope' do
+    it { expect(Todo.all.newest).to contain_exactly(second_todo, first_todo) }
   end
 end
