ad1fa1e274c1d04ca456ea445390dfa10e1e1308
Joe Palmieri <jpalmieri@lumoslabs.com>
2016-06-27 22:38:53 -0700

    Order todos by newest first (#58)
    * add :newest scope to todos
    
    * order todos by newest on list#show and todo#index

--- spec/features/user_creates_todos_spec.rb
+++ spec/features/user_creates_todos_spec.rb
@@ -8,12 +8,12 @@ feature 'User creates todos', js: true do
     list = create(:list, user: user)
     visit list_path 1
 
-    fill_in 'Description', with: "Buy groceries"
+    fill_in 'Description', with: 'first todo'
     within(:css, 'form.add-todo') do
       click_button '+'
     end
 
-    fill_in 'Description', with: "Get haircut"
+    fill_in 'Description', with: 'second todo'
     within(:css, 'form.add-todo') do
       click_button '+'
     end
@@ -22,9 +22,10 @@ feature 'User creates todos', js: true do
   scenario "successfully" do
 
     expect(current_path).to eq(list_path 1)
-
-    expect( page ).to have_content("Buy groceries")
-    expect( page ).to have_content("Get haircut")
+    expect( page ).to have_css(
+      '#todos tr:first-child', text: 'second todo')
+    expect( page ).to have_css(
+      '#todos tr:last-child', text: 'first todo')
   end
 
   scenario 'Unsuccessfully: no description' do
