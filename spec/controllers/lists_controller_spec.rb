9147165278c1584405696f79deeeeeb308ce147b
jpalmieri <jpalmieri@lumoslabs.com>
2016-07-01 21:57:03 -0700

    use let instead of before in controller specs

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,26 +1,23 @@
 require 'rails_helper'
 
 describe ListsController do
-
   include Devise::TestHelpers
 
-  before do
-    @user1 = create(:user)
-    @user2 = create(:user)
-    @list = create(:list, user: @user1)
-  end
+  let!(:user1) { create(:user) }
+  let!(:user2) { create(:user) }
+  let(:list)   { create(:list, user: user1) }
 
   describe '#destroy' do
     it "allows a user to delete own list" do
-      sign_in @user1
-      delete :destroy, { id: @list.id }
+      sign_in user1
+      delete :destroy, { id: list.id }
       is_expected.to redirect_to lists_url
       is_expected.to set_the_flash.to("List deleted successfully")
     end
 
     it "does not allow a user to delete another user's list" do
-      sign_in @user2
-      delete :destroy, { id: @list.id }
+      sign_in user2
+      delete :destroy, { id: list.id }
       is_expected.to redirect_to root_url
       is_expected.to set_the_flash.to("There was an error deleting your list. You are not authorized to delete this list.")
     end
--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,26 +1,23 @@
 require 'rails_helper'
 
 describe TodosController do
-
   include Devise::TestHelpers
 
-  before do
-    @user1 = create(:user)
-    @user2 = create(:user)
-    @todo = create(:todo, user: @user1)
-  end
+  let!(:user1) { create(:user) }
+  let!(:user2) { create(:user) }
+  let(:todo)   { create(:todo, user: user1) }
 
   describe '#destroy' do
     it "allows a user to delete own todo" do
-      sign_in @user1
-      delete :destroy, { id: @todo.id }
+      sign_in user1
+      delete :destroy, { id: todo.id }
       is_expected.to redirect_to todos_url
       is_expected.to set_the_flash.to("Todo deleted successfully")
     end
 
     it "does not allow a user to delete another user's todo" do
-      sign_in @user2
-      delete :destroy, { id: @todo.id }
+      sign_in user2
+      delete :destroy, { id: todo.id }
       is_expected.to redirect_to root_url
       is_expected.to set_the_flash.to("There was an error deleting your todo. You are not authorized to delete this todo.")
     end
