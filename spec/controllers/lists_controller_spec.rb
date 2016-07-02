069735532de2f66941651bc0ba7275fea832a0dc
jpalmieri <jpalmieri@lumoslabs.com>
2016-07-01 22:16:21 -0700

    clarify controller specs, only sign in one user each

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -3,23 +3,24 @@ require 'rails_helper'
 describe ListsController do
   include Devise::TestHelpers
 
-  let!(:user1) { create(:user) }
-  let!(:user2) { create(:user) }
-  let(:list)   { create(:list, user: user1) }
+  let!(:signed_in_user)  { create(:user) }
+  let!(:other_user)      { create(:user) }
+  let(:accessible_list)  { create(:list, user: signed_in_user) }
+  let(:other_users_list) { create(:list, user: other_user) }
+
+  before { sign_in signed_in_user }
 
   describe '#destroy' do
     it "allows a user to delete own list" do
-      sign_in user1
-      delete :destroy, { id: list.id }
+      delete :destroy, { id: accessible_list.id }
       is_expected.to redirect_to lists_url
-      is_expected.to set_the_flash.to("List deleted successfully")
+      is_expected.to set_the_flash.to('List deleted successfully')
     end
 
     it "does not allow a user to delete another user's list" do
-      sign_in user2
-      delete :destroy, { id: list.id }
+      delete :destroy, { id: other_users_list.id }
       is_expected.to redirect_to root_url
-      is_expected.to set_the_flash.to("There was an error deleting your list. You are not authorized to delete this list.")
+      is_expected.to set_the_flash.to('There was an error deleting your list. You are not authorized to delete this list.')
     end
 
   end
--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -3,23 +3,24 @@ require 'rails_helper'
 describe TodosController do
   include Devise::TestHelpers
 
-  let!(:user1) { create(:user) }
-  let!(:user2) { create(:user) }
-  let(:todo)   { create(:todo, user: user1) }
+  let!(:signed_in_user)  { create(:user) }
+  let!(:other_user)      { create(:user) }
+  let(:accessible_todo)  { create(:todo, user: signed_in_user) }
+  let(:other_users_todo) { create(:todo, user: other_user) }
+
+  before { sign_in signed_in_user }
 
   describe '#destroy' do
-    it "allows a user to delete own todo" do
-      sign_in user1
-      delete :destroy, { id: todo.id }
+    it 'allows a user to delete own todo' do
+      delete :destroy, { id: accessible_todo.id }
       is_expected.to redirect_to todos_url
-      is_expected.to set_the_flash.to("Todo deleted successfully")
+      is_expected.to set_the_flash.to('Todo deleted successfully')
     end
 
     it "does not allow a user to delete another user's todo" do
-      sign_in user2
-      delete :destroy, { id: todo.id }
+      delete :destroy, { id: other_users_todo.id }
       is_expected.to redirect_to root_url
-      is_expected.to set_the_flash.to("There was an error deleting your todo. You are not authorized to delete this todo.")
+      is_expected.to set_the_flash.to('There was an error deleting your todo. You are not authorized to delete this todo.')
     end
 
   end
