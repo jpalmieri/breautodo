5f5bbeede1aaebf87ac591ec2a25cc47e3be2a67
jpalmieri <jpalmieri@lumoslabs.com>
2016-07-01 22:52:15 -0700

    use let syntax in feature specs, use single- instead of double-quotes

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-describe "Sign in flow" do
+feature 'User signs in' do
+  let(:user) { create(:user) }
+  before { sign_in(user) }
 
-  describe "successful" do
-    it "redirects to the todo index" do
-      user = create(:user)
-      sign_in(user)
-
+  describe 'successful' do
+    it 'redirects to the todo index' do
       expect(current_path).to eq lists_path
       expect(page).to_not have_css(".nav", text: "Sign In")
       expect(page).to have_css(".nav", text: user.email)
