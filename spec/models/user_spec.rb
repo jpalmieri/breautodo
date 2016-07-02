84f3698420200b3c424ec083381137764f46a23b
jpalmieri <jpalmieri@lumoslabs.com>
2016-07-01 20:40:16 -0700

    refactor user model spec, update syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
 describe User do
+  let(:user) { build(:user) }
 
-  context "associations" do
-    it { expect(User.new).to have_many(:todos) }
+  context 'associations' do
+    it { should have_many(:todos) }
   end
 
-  context "has auth_token" do
-    it "adds an auth_token to new user" do
-      user = build(:user)
+  context 'has auth_token' do
+    it 'adds an auth_token to new user' do
       expect(user.auth_token).to be_nil
       user.save!
       expect(user.auth_token).to match(/\A[a-zA-Z0-9_-]{20}\z/)
     end
 
-    it "calls Devise.friendly_token on user#create" do
-      user = build(:user)
+    it 'calls Devise.friendly_token on user#create' do
       expect(user.auth_token).to be_nil
-
-      expect(Devise).to receive(:friendly_token).and_return "some-secret-token"
-
+      expect(Devise).to receive(:friendly_token).and_return 'some-secret-token'
       user.save!
-      expect(user.auth_token).to eq("some-secret-token")
+      expect(user.auth_token).to eq('some-secret-token')
     end
 
-    it "returns error when auth token is removed from user" do
-      user = create(:user)
+    it 'returns error when auth token is removed from user' do
+      user.save!
       user.auth_token = nil
       user.valid?
-      expect(user.errors.full_messages).to eq(["Auth token can't be blank"])
+      expect(user.errors.full_messages).to eq(['Auth token can\'t be blank'])
     end
   end
 end
