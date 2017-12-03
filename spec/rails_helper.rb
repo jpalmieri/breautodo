86f34249d533195fcf2e638773a2b3e0e3ee8f3a
Joe Palmieri <jpalmieri@lumoslabs.com>
2017-12-02 16:00:30 -0800

    addresss devise test helpers depreciation warnings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,9 @@ RSpec.configure do |config|
   config.include(EmailSpec::Helpers)
   config.include(EmailSpec::Matchers)
 
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
